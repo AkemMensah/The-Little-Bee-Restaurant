4e124b5e974691ccb26ff06e6bae29ab81538e54
Manuel <141195emmanuel@gmail.com>
2023-11-29 15:10:47 +0000

    added images

---
+++ little_bee_frontend/src/components/images/index.js
@@ -0,0 +1,16 @@
+import greek from "./greek.jpg"
+import hero_cake from "./hero_cake.jpg"
+import lemonWord from "./lemonWord.jpg"
+import lemon_fish from "./lemon_fish.jpg"
+import tomato_cake from "./tomato_cake.jpg"
+import matheus from "./matheus.jpg"
+import joseph from "./joseph.jpg"
+import michael from "./michael.jpg"
+import christina from './christina.jpg'
+import couples from './couples.png'
+import lemon from './lemon.jpg'
+import vegetables from './vegetables.jpg'
+import orderPack from './orderPack.jpg'
+
+
+export {greek, hero_cake,lemonWord,lemon_fish,tomato_cake,vegetables,matheus,michael,joseph,christina,couples,lemon}
