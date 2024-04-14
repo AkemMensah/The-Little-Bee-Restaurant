7466eb0949eca38848fa8852a5e44d942ac98c65
AkemMensah <141175emmanuel@gmail.com>
2024-04-14 18:03:18 +0000

    changed the whole outlook of the landing page

--- little_bee_frontend/src/components/LandingPage.js
+++ little_bee_frontend/src/components/LandingPage.js
@@ -1,29 +1,25 @@
 import React from "react";
-import { littleBee } from "../images";
-import {
-    reserve,
-    homeshot,
-    confirm1,
-    confirm2,
-    order,
-} from "../images/index.js";
+import * as images from '../images/index';
 import "../styles/landingPage.css";
-import Footer from "./Footer";
 import { useNavigate } from "react-router-dom";
-import socialLinks from './Footer'
+import Carousel from 'react-bootstrap/Carousel';
+import CarouselComp from "./Carousel.js"
+
+
 
 // Defining nav bar for the landing page
-function Nav() {
+function Nav()
+{
     const navigate = useNavigate();
-
-    const handleClickHome = () => {
+    const handleClickHome = () =>
+    {
         navigate("/home");
     };
     return (
         <nav className="navigate">
             <div className="logo">
                 <img
-                    src={littleBee}
+                    src={images.littleBee}
                     alt="little Bee"
                     className="app-logo"
                     width="90"
@@ -33,13 +29,22 @@ function Nav() {
             </div>
             <ul className="llinks">
                 <li>
+                    <a href="/home">Home</a>
+                </li>
+                <li>
+                    <a href="/home">Locations</a>
+                </li>
+                <li>
+                    <a href="/home">Dine-in</a>
+                </li>
+                <li>
+                    <a href="/order-form">Delivery</a>
+                </li>
+                <li className='reservations'>
+                    <a href="/booking-form">Reservations</a>
+                </li>
+                <li className='order-online'>
                     <a href="/order-form">Order Online</a>
-                </li>
-                <li>
-                    <a href="/booking-form">Reserve A Table</a>
-                </li>
-                <li>
-                    <a href="/home">Home Page</a>
                 </li>
             </ul>
         </nav>
@@ -47,16 +52,12 @@ function Nav() {
 }
 
 // Defining introductory text for the banner
-function Intro() {
+function Intro()
+{
     return (
         <div className="intro">
             <div className="intro-text">
-                <h1>Discover The World of Little Bee Restaurant</h1>
-                <p>
-                    A portfolio project Designed to ensure the online presence
-                    of the little Bee experience allowing customers to order
-                    meals and book dables online.
-                </p>
+                <h1>Savor the Taste, Indulge in the Experience: Welcome to Culinary Delights!</h1>
             </div>
         </div>
     );
@@ -64,7 +65,8 @@ function Intro() {
 
 
 // Defining the header for the landing page
-function LHeading() {
+function LHeading()
+{
     return (
         <div className="lheading">
             <Nav />
@@ -74,82 +76,43 @@ function LHeading() {
 }
 
 // Defining the body of the landing page
-function LBody() {
+function LBody()
+{
     return (
         <>
-            <div className="body">
-                <div>
-                    <h2>Online Ordering Feature</h2>
-                    <img src={order} />
-                    <p>
-                        This feature allows customers to order food on the
-                        Little Bee web site which can be delivered to them in
-                        the shortest possible time at a customer's request.
-                    </p>
+            <CarouselComp />
+
+            {/* <img src={images.brunch} /> */}
+
+            {/* <section className="body">
+                <div className="Carousel.Item">
+                    <h2>Breakfast</h2>
+                    <img src={images.breakfast1} />
+
                 </div>
-                <div>
-                    <h2>Table Reservation Feature</h2>
-                    <img src={reserve} />
-                    <p>
-                        This feature allows customers to reserve a table for
-                        their various occasionson the Little Bee web site. The
-                        customer can choose a date and time and the tabel will
-                        be reserved a ccording to the request.
-                    </p>
+                <div className="Carousel.Item">
+                    <h2>Lunch</h2>
+                    <img src={images.lunch} />
+
                 </div>
-                <div>
-                    <h2>The Home Page Feature</h2>
-                    <img src={homeshot} />
-                    <p>
-                        This is the home page for the Little Bee restaurant. It
-                        has various features to allow for various
-                        functionalities in accessing various aspects of the web
-                        application. Among the features are: The reserve a table
-                        button, order online button etc.
-                    </p>
+                <div className="Carousel.Item">
+                    <h2>Dinner</h2>
+                    <img src={images.diner} />
+
                 </div>
-                <div>
-                    <h2>Confirmation Feature</h2>
-                    <img src={confirm1} />
-                    <p>
-                        This feature allows customers to know if their requests
-                        were successful after they order a menu online or
-                        reserve a table on the Little Bee website.
-                    </p>
+                <div className="Carousel.Item">
+                    <h2>Brunch</h2>
+                    <img src={images.snacks} alt='snacks' />
+
                 </div>
-            </div>
-            <div className="labout">
-                <div className="text1">
-                    <div className="text2">
-                        <h1>The project's inspiration</h1>
-                        <p>
-                            In the midst of my Holberton School journey, a spark
-                            ignited—an idea for a Portfolio Project that
-                            transcended a mere checkbox. It wasn't just about
-                            meeting requirements; it was an opportunity to weave
-                            my story into code. As days turned to weeks, lines
-                            of code danced, forming the backbone of the project.
-                            Challenges weren't just technical; they were
-                            opportunities for deeper problem-solving. Bugs fixed
-                            and features added became milestones in my
-                            evolution. This project mirrored my growth,
-                            reflecting the dedication to crafting software that
-                            transcends functionality. It echoed my commitment to
-                            the craft and the holistic learning experience at
-                            Holberton. With lines of code neatly arranged, the
-                            Portfolio Project came to life. It stood as a bridge
-                            between theoretical knowledge and practical skills,
-                            a testament to the transformative power of education
-                            and the limitless possibilities that unfold when
-                            creativity meets code. To delve into the heart of my
-                            journey, explore the project here. It's more than a
-                            project; it's a story waiting to be told.
-                        </p>
-                    </div>
-                    <div className="connect">
-                        <h1>Let's Conect on:</h1>
-                        <ul>
-                            {/* {socialLinks.map((item, index) => {
+                <div className="Carousel.Item">
+                    <h2>A La Carte</h2>
+                    <img src={images.chicken} alt='snacks' />
+                </div>
+            </section> */}
+            
+            {/* <ul> */}
+            {/* {socialLinks.map((item, index) => {
                                 return (
                                     <li key={index}>
                                         <a href={item.url} target="_blank">
@@ -158,40 +121,19 @@ function LBody() {
                                     </li>
                                 );
                             })} */}
-                            <li>Github</li>
-                            <li>LinkedIn</li>
-                            <li>Twiter</li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="video">
-                    <h1> project video </h1>
-                    <iframe
-                        width="760"
-                        height="500"
-                        src="https://https://youtu.be/p7s10g6ioV8"
-                        title="YouTube video player"
-                        frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        allowfullscreen
-                    ></iframe>
-                    {/* <video src={video} */}
-                    {/* controls */}
-                    {/* // width="900" */}
-                    {/* // height="200" */}
-                    {/* /> */}
-                </div>
-            </div>
+
+            {/* </ul> */}
         </>
     );
 }
 
 // Defining the landing page component
-function LandingPage() {
+function LandingPage()
+{
     return (
         <div id="landing-page-container">
             <LHeading />
-            <h1>App Features</h1>
+            <h1>Our Meals Category</h1>
             <LBody />
         </div>
     );
