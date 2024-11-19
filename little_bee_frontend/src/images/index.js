8ea06d5f147f61ce36f90dfd0b5fda669c0dc68c
AkemMensah <141195emmanuel@gmail.com>
2024-11-19 13:39:02 +0000

    removed redundant files

--- little_bee_frontend/src/images/index.js
+++ little_bee_frontend/src/images/index.js
@@ -1,24 +1,11 @@
 import greek from "./greek.jpg";
-import hero_cake from "./hero_cake.jpg";
-import lemonWord from "./lemonWord.jpg";
 import lemon_fish from "./lemon_fish.jpg";
 import tomato_cake from "./tomato_cake.jpg";
-import matheus from "./matheus.jpg";
-import joseph from "./joseph.jpg";
-import michael from "./michael.jpg";
-import christina from "./christina.jpg";
-import couples from "./couples.png";
-import lemon from "./lemon.jpg";
 import vegetables from "./vegetables.jpg";
 import orderPack from "./orderPack.jpg";
 import littleBee from "./little_Bee.jpg";
 import restaurantBanner from "./Restaurant-interior.jpg";
 import Bee from "./Bee.jpg";
-import homeshot from "./Screenshot17.png";
-import reserve from "./Screenshot18.png";
-import order from "./Screenshot19.png";
-import confirm1 from "./Screenshot21.png";
-import confirm2 from "./Screenshot23.png";
 import diner from "./diner.jpg";
 import lunch from "./lunch.jpg";
 import breakfast from "./all_images/breakfast1.jpg";
@@ -26,35 +13,20 @@ import snacks from "./snacks.jpg";
 import breakfast1 from "./lovely_breakfast.jpg";
 import brunch from "./lovely_brunch.jpg";
 import chicken from "./chicken.jpg";
-import ali from "./all_images/ali-morshedlou-WMD64tMfc4k-unsplash.jpg";
 import bowls from "./bowls-of-food.jpg";
 import Prince from "./prince.jpg";
 import food from "./food2.jpg"
 
 
-
 export {
     greek,
-    hero_cake,
-    lemonWord,
     lemon_fish,
     tomato_cake,
     orderPack,
     vegetables,
-    matheus,
-    michael,
-    joseph,
-    christina,
-    couples,
-    lemon,
     littleBee,
     Bee,
     restaurantBanner,
-    homeshot,
-    order,
-    confirm1,
-    confirm2,
-    reserve,
     breakfast,
     snacks,
     lunch,
@@ -62,7 +34,6 @@ export {
     breakfast1,
     brunch,
     chicken,
-    ali,
     bowls,
     Prince,
     food
